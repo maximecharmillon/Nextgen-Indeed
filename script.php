85dc51ad3f06d705df1cfd874548791b74911328
Parisi <maxime.parisi@epitech.eu>
2023-10-11 18:35:51 +0200

    co php/bdd

--- script.php
+++ script.php
@@ -1,5 +1,29 @@
 <?php
+// connexion php/sql 
 
+$serveur= "localhost";
+$user= "parisi";
+$pwd= "22222Mm@";
+$db= "NEXTGEN";
+
+$co= new mysqli($serveur, $user, $pwd, $db);
+
+if( $co-> connect_error)
+{
+    die("errreeuuurrrrr");
+}
+else
+{
+    echo "la co est bonne\n";
+}
+
+
+
+$SELCECTid= "SELECT JOB_ID FROM ANNONCES";
+echo $SELCECTid;
+
+
+// connexion js/php
 header("Access-Control-Allow-Origin: *");
 
 $jobId= 44;
