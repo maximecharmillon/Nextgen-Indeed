4f1058ac83b1c52c130641e7bc7b4dec701bd8a3
Parisi <maxime.parisi@epitech.eu>
2023-10-13 16:38:52 +0200

    add file mvc

--- mvc/view/v.js
+++ mvc/view/v.js
@@ -1,14 +1,19 @@
-form = new FormData;
-form.append("USER", "max123")
+let user= new FormData
+user.append("user", "max")
+user.append("pwd", 2)
+user.append("email", "mpointfr")
+user.append("userId", 4)
+user.append("jobId", 46)
 
 fetch("http://localhost/Nextgen-Indeed/mvc/controler/c.php",{
     method: "POST",
     mode: "cors",
-    body: form
-}).then((r)=> {return r.text()})
-.then((body)=> {
+    body: user
+}).then((r)=> { return r.text()
+}).then((body)=> {
     console.log(body);
     let text= document.createElement("p");
     text.innerHTML= body;
     document.querySelector("div").appendChild(text);
 })
+
