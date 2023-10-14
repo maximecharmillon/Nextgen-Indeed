eb0918d8bba5eae3ddcb7113c4a489110ee6601e
Parisi <maxime.parisi@epitech.eu>
2023-10-14 08:26:04 +0200

    requette get

--- mvc/view/v.js
+++ mvc/view/v.js
@@ -1,19 +1,37 @@
-let user= new FormData
-user.append("user", "max")
-user.append("pwd", 2)
-user.append("email", "mpointfr")
-user.append("userId", 4)
-user.append("jobId", 46)
+let insertUser= new FormData
+insertUser.append("user", "max")
+insertUser.append("pwd", 2)
+insertUser.append("email", "mpointfr")
+insertUser.append("userId", 4)
+insertUser.append("jobId", 46)
 
-fetch("http://localhost/Nextgen-Indeed/mvc/controler/c.php",{
+
+let link= "http://localhost/Nextgen-Indeed/mvc/controler/c.php";
+
+fetch(link,{
     method: "POST",
     mode: "cors",
-    body: user
+    body: insertUser
 }).then((r)=> { return r.text()
 }).then((body)=> {
     console.log(body);
-    let text= document.createElement("p");
-    text.innerHTML= body;
-    document.querySelector("div").appendChild(text);
+    let postUser= document.createElement("p");
+    postUser.innerHTML= body;
+    document.querySelector("div").appendChild(postUser);
+})
+
+
+let selectUser= 1
+
+fetch(link+ "?userId="+ encodeURIComponent(selectUser), {
+    method: "GET",
+    mode: "cors",
+    
+}).then((r)=> { 
+    r.text()
+}).then((body)=> {
+    let getUser= document.createElement("p")
+    getUser.innerHTML= "Bonjour "+ body;
+    document.querySelector("div").appendChild(getUser)
 })
 
