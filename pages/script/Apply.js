c93f8159a43b7a36ff16b16955da236a7d74b9ef
Jean-francis <jean-francis.achard@epitech.eu>
2023-10-16 11:18:40 +0200

    j'ai fait learn more

--- pages/script/Apply.js
+++ pages/script/Apply.js
@@ -6,3 +6,15 @@ function toggleForm(formId) {
         form.style.display = 'none';
     }
 }
+
+function toggleInfo(infoId, shortDescId) {
+    const infoDiv = document.getElementById(infoId);
+    const shortDesc = document.getElementById(shortDescId);
+    if (infoDiv.style.display === "none") {
+        infoDiv.style.display = "block";
+        shortDesc.style.display = "none";
+    } else {
+        infoDiv.style.display = "none";
+        shortDesc.style.display = "block";
+    }
+}
