f5341d0552b6e156c635c01eedecc182beef66f6
Parisi <maxime.parisi@epitech.eu>
2023-10-16 18:41:24 +0200

    task03 done

--- pages/script/salary.js
+++
@@ -1,7 +0,0 @@
-const salaryRange = document.getElementById("salaryRange");
-  const selectedSalary = document.getElementById("selectedSalary");
-
-  salaryRange.addEventListener("input", function() {
-    const selectedValue = parseInt(salaryRange.value);
-    selectedSalary.textContent = `Select a salary : $${selectedValue}/month`;
-});
